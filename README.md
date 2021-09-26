073d6fc77f5264c1c04b516e28f88899ff28cd4b
Kristopher Buote <krisbuote@gmail.com>
2021-09-26 15:57:53 -0700

    Updated comments and README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # covalent-task
 Covalent Take Home Exercise
+Kristopher Buote
+
+# TASK 
+- Write a program that queries Covalent's API ticker endpoint. 
+- Collect null contract address results for 60 samples at a rate of 1 sample per minute. 
+- After 60 samples, display a histogram of null results. Only include currencies that have >0 null address responses.
+
--- covalent-task.py
+++ covalent-task.py
@@ -1,19 +1,25 @@
 ### TASK ###
 # Write a program that queries Covalent's API ticker endpoint. 
-# Collect null address results for 60 samples at a rate of 1 sample per minute. 
-# After 60 samples, display a histogram of null results. Only include currencies that had >0 null address responses.
+# Collect null contract address results for 60 samples at a rate of 1 sample per minute. 
+# After 60 samples, display a histogram of null results. Only include currencies that have >0 null address responses.
 
-### Author
+### AUTHOR ###
 # Kristopher Buote
 # September 26, 2021
 
-### Use python's request library to query API
+### Use python's requests library to query API
 import requests
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import sys
+
+### SET UP ###
+# Test connection to the Covalent API with error handling. 
+# Organize the response data into dictionaries.
+# Create a dictionary to store the contract addresses from the API response.
+# This program uses the Ticker to associate with sample responses. 
 
 ### Receive API key as user input
 mykey = input("Enter Covalent API Key:")
@@ -22,12 +28,13 @@ mykey = input("Enter Covalent API Key:")
 try:
     response = requests.get('https://api.covalenthq.com/v1/pricing/tickers/',
                             auth=(mykey, ''))
+
 # Exit program if initial connection failed.
 except:
-    print(f"Error in request. Proram exited. Error:{sys.exc_info()[0]}")
+    print(f"Connection error. Program exited. Error:{sys.exc_info()[0]}")
     exit()
 
-### Collect the API response's error and message
+### Collect the API's response error and message
 json_response = response.json()
 error = json_response["error"]
 error_message = json_response["error_message"]
@@ -37,7 +44,7 @@ error_message = json_response["error_message"]
 if response.status_code == 200 and error is False:
     print(f"API Response: {response.status_code}. No error. Program continuing.")
 
-    ### Organize API response JSON data into dictionaries
+    ### Organize API response JSON data
     data = json_response["data"]
     timestamp = data["updated_at"]
     items = data["items"]
@@ -56,7 +63,11 @@ else:
     exit()
 
 
-### Collect 60 samples, approximately 1 sample per minute
+### DATA COLLECTION ###
+# Collect 60 samples, approximately 1 sample per minute
+# The results are stored in a nested dictionary using the Ticker and sample count as keys.
+# e.g. {'Ticker1': {1: 'abcd', 2: null, ..., 60: 'abcd'}, 'Ticker2': {1: 'efg', 2: 'efg', ..., 60: null}}
+
 for sample in range(1, 61):
 
     # Query API
@@ -67,12 +78,10 @@ for sample in range(1, 61):
     error = json_response["error"]
     error_message = json_response["error_message"]
     
-    # Organize API response JSON data into dictionaries
+    # Organize API response JSON data
     data = json_response["data"]
     timestamp = data["updated_at"]
     all_items = data["items"]
-
-    #print(timestamp)
     
     ### Check if an error was thrown by the API response
     # If error message is false, continue with program. Else, skip sample.
@@ -84,27 +93,17 @@ for sample in range(1, 61):
             item_ticker = item["contract_ticker_symbol"]
             item_address = item["contract_address"]
             results[item_ticker][sample] = item_address
+            
+        print(f"Sample {sample} collected.")
         
     else:
         print(f"Sample {sample} failed and skipped. API error message: {error_message}")
 
     # Wait 1 minute for another sample. This is usually commented out for developing.
-    # time.sleep(60)
+    time.sleep(60)
 
-# MUST REMOVE
-##
-### KRIS ADDED NULL INTO RESULTS TO TEST
-results["WETH"][60] = None
-results["WETH"][1] = None
-results["WETH"][3] = None
-results["USDT"][60] = None
-results["USDT"][54] = None
-results["BNB"][3] = None
-###
-##
-#
-
-### Count the amount of null responses for each ticker across the 60 samples
+### DATA ANALYSIS ###
+# Count the amount of null responses for each ticker across the 60 samples
 # Note that None is the variable for null in python
 
 # Create a new dictionary to store the null count
@@ -121,7 +120,7 @@ ticker_null_count_non_zero = {key: value for key, value in ticker_null_count.ite
 # Sort the dictionary in ascending order
 ticker_null_count_ordered = {key: value for key, value in sorted(ticker_null_count_non_zero.items(), key=lambda item: item[1])}
 
-
+### DATA PRESENTATION AND SAVING ###
 ### Save the null count data to a csv file
 with open('null_count.csv', 'w') as csv_file:  
     writer = csv.writer(csv_file)
@@ -133,8 +132,12 @@ plt.bar(ticker_null_count_ordered.keys(), ticker_null_count_ordered.values(), wi
 plt.title("Null Address Count over 60 samples")
 plt.xlabel("Currency Ticker")
 plt.ylabel("Null Address Count")
+
+# Save the figure
 plt.savefig('null_count_histogram.png')
 plt.show()
+
+print("Program complete.")
 
 
 
